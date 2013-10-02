235338092824fdfc6e1846673df9b8b1cc7531e5
Andrii Koniushenko <andre.koniushenko@gmail.com>
2013-10-03 00:50:32 +0300

    set up

---
+++ VirtualGym/DataModel/DataSource.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.ApplicationModel.Resources.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+using Windows.Web.Syndication;
+using System.Text.RegularExpressions;
+using System.IO;
+
+// The data model defined by this file serves as a representative example of a strongly-typed
+// model that supports notification when members are added, removed, or modified.  The property
+// names chosen coincide with data bindings in the standard item templates.
+//
+// Applications may use this model as a starting point and build on it, or discard it entirely and
+// replace it with something appropriate to their needs.
+
+namespace VirtualGym.Data
+{
+    /// <summary>
+    /// Base class for <see cref="DataItem"/> and <see cref="DataGroup"/> that
+    /// defines properties common to both.
+    /// </summary>
+    [Windows.Foundation.Metadata.WebHostHidden]
+    public abstract class DataCommon : VirtualGym.Common.BindableBase
+    {
+        private static Uri _baseUri = new Uri("ms-appx:///");
+
+        public DataCommon(String uniqueId, String title, String subtitle, String imagePath, String description)
+        {
+            this._uniqueId = uniqueId;
+            this._title = title;
+            this._subtitle = subtitle;
+            this._description = description;
+            this._imagePath = imagePath;
+        }
+
+        private string _uniqueId = string.Empty;
+        public string UniqueId
+        {
+            get { return this._uniqueId; }
+            set { this.SetProperty(ref this._uniqueId, value); }
+        }
+
+        private string _title = string.Empty;
+        public string Title
+        {
+            get { return this._title; }
+            set { this.SetProperty(ref this._title, value); }
+        }
+
+        private string _subtitle = string.Empty;
+        public string Subtitle
+        {
+            get { return this._subtitle; }
+            set { this.SetProperty(ref this._subtitle, value); }
+        }
+
+        private string _description = string.Empty;
+        public string Description
+        {
+            get { return this._description; }
+            set { this.SetProperty(ref this._description, value); }
+        }
+
+        private ImageSource _image = null;
+        private String _imagePath = null;
+        public ImageSource Image
+        {
+            get
+            {
+                if (this._image == null && this._imagePath != null)
+                {
+                    this._image = new BitmapImage(new Uri(DataCommon._baseUri, this._imagePath));
+                }
+                return this._image;
+            }
+
+            set
+            {
+                this._imagePath = null;
+                this.SetProperty(ref this._image, value);
+            }
+        }
+
+        public void SetImage(String path)
+        {
+            this._image = null;
+            this._imagePath = path;
+            this.OnPropertyChanged("Image");
+        }
+
+        public override string ToString()
+        {
+            return this.Title;
+        }
+    }
+
+    /// <summary>
+    /// Generic item data model.
+    /// </summary>
+    public class DataItem : DataCommon
+    {
+        public DataItem(String uniqueId, String title, String subtitle, String imagePath, String description, String content, DataGroup group)
+            : base(uniqueId, title, subtitle, imagePath, description)
+        {
+            this._content = content;
+            this._group = group;
+        }
+
+        private string _content = string.Empty;
+        public string Content
+        {
+            get { return this._content; }
+            set { this.SetProperty(ref this._content, value); }
+        }
+
+        private DataGroup _group;
+        public DataGroup Group
+        {
+            get { return this._group; }
+            set { this.SetProperty(ref this._group, value); }
+        }
+    }
+
+    /// <summary>
+    /// Generic group data model.
+    /// </summary>
+    public class DataGroup : DataCommon
+    {
+        public DataGroup(String uniqueId, String title, String subtitle, String imagePath, String description)
+            : base(uniqueId, title, subtitle, imagePath, description)
+        {
+            Items.CollectionChanged += ItemsCollectionChanged;
+        }
+
+        private void ItemsCollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            // Provides a subset of the full items collection to bind to from a GroupedItemsPage
+            // for two reasons: GridView will not virtualize large items collections, and it
+            // improves the user experience when browsing through groups with large numbers of
+            // items.
+            //
+            // A maximum of 12 items are displayed because it results in filled grid columns
+            // whether there are 1, 2, 3, 4, or 6 rows displayed
+
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    if (e.NewStartingIndex < 12)
+                    {
+                        TopItems.Insert(e.NewStartingIndex,Items[e.NewStartingIndex]);
+                        if (TopItems.Count > 12)
+                        {
+                            TopItems.RemoveAt(12);
+                        }
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    if (e.OldStartingIndex < 12 && e.NewStartingIndex < 12)
+                    {
+                        TopItems.Move(e.OldStartingIndex, e.NewStartingIndex);
+                    }
+                    else if (e.OldStartingIndex < 12)
+                    {
+                        TopItems.RemoveAt(e.OldStartingIndex);
+                        TopItems.Add(Items[11]);
+                    }
+                    else if (e.NewStartingIndex < 12)
+                    {
+                        TopItems.Insert(e.NewStartingIndex, Items[e.NewStartingIndex]);
+                        TopItems.RemoveAt(12);
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    if (e.OldStartingIndex < 12)
+                    {
+                        TopItems.RemoveAt(e.OldStartingIndex);
+                        if (Items.Count >= 12)
+                        {
+                            TopItems.Add(Items[11]);
+                        }
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    if (e.OldStartingIndex < 12)
+                    {
+                        TopItems[e.OldStartingIndex] = Items[e.OldStartingIndex];
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    TopItems.Clear();
+                    while (TopItems.Count < Items.Count && TopItems.Count < 12)
+                    {
+                        TopItems.Add(Items[TopItems.Count]);
+                    }
+                    break;
+            }
+        }
+
+        private ObservableCollection<DataItem> _items = new ObservableCollection<DataItem>();
+        public ObservableCollection<DataItem> Items
+        {
+            get { return this._items; }
+        }
+
+        private ObservableCollection<DataItem> _topItem = new ObservableCollection<DataItem>();
+        public ObservableCollection<DataItem> TopItems
+        {
+            get {return this._topItem; }
+        }
+    }
+
+    /// <summary>
+    /// Creates a collection of groups and items with hard-coded content.
+    /// 
+    /// SampleDataSource initializes with placeholder data rather than live production
+    /// data so that sample data is provided at both design-time and run-time.
+    /// </summary>
+    public sealed class DataSource
+    {
+
+        public static readonly ObservableCollection<DataGroup> AllGroups = new ObservableCollection<DataGroup>();
+
+        public static IEnumerable<DataGroup> GetGroups(string uniqueId)
+        {
+            if (!uniqueId.Equals("AllGroups")) throw new ArgumentException("Only 'AllGroups' is supported as a collection of groups");
+            
+            return AllGroups;
+        }
+
+        public static DataGroup GetGroup(string uniqueId)
+        {
+            // Simple linear search is acceptable for small data sets
+            var matches = AllGroups.Where((group) => group.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+
+        public static DataItem GetItem(string uniqueId)
+        {
+            // Simple linear search is acceptable for small data sets
+            var matches = AllGroups.SelectMany(group => group.Items).Where((item) => item.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+
+        public static async Task<bool> AddGroupForFeedAsync(string feedUrl)
+        {
+            string clearedContent = String.Empty;
+
+            if (DataSource.GetGroup(feedUrl) != null) return false;
+
+            var feed = await new SyndicationClient().RetrieveFeedAsync(new Uri(feedUrl));
+
+            var feedGroup = new DataGroup(
+                uniqueId: feedUrl,
+                title: feed.Title != null ? feed.Title.Text : null,
+                subtitle: feed.Subtitle != null ? feed.Subtitle.Text : null,
+                imagePath: feed.ImageUri != null ? feed.ImageUri.ToString() : null,
+                description: null);
+
+            foreach (var i in feed.Items)
+            {
+                string imagePath = GetImageFromPostContents(i);
+
+                if (i.Summary != null)
+                    clearedContent = Windows.Data.Html.HtmlUtilities.ConvertToText(i.Summary.Text);
+                else
+                    if (i.Content != null)
+                        clearedContent = Windows.Data.Html.HtmlUtilities.ConvertToText(i.Content.Text);
+
+                if (imagePath != null && feedGroup.Image == null)
+                    feedGroup.SetImage(imagePath);
+
+                if (imagePath == null) imagePath = "ms-appx:///Assets/DarkGray.png";
+
+                feedGroup.Items.Add(new DataItem(
+                    uniqueId: i.Id, title: i.Title.Text, subtitle: null, imagePath: imagePath,
+                    description: null, content: clearedContent, @group: feedGroup));
+            }
+
+            AllGroups.Add(feedGroup);
+            return true;
+        }
+
+        private static string GetImageFromPostContents(SyndicationItem item)
+        {
+            string text2search = "";
+
+            if (item.Content != null) text2search += item.Content.Text;
+            if (item.Summary != null) text2search += item.Summary.Text;
+
+            return Regex.Matches(text2search,
+                    @"(?<=<img\s+[^>]*?src=(?<q>['""]))(?<url>.+?)(?=\k<q>)",
+                    RegexOptions.IgnoreCase)
+                .Cast<Match>()
+                .Where(m =>
+                {
+                    Uri url;
+                    if (Uri.TryCreate(m.Groups[0].Value, UriKind.Absolute, out url))
+                    {
+                        string ext = Path.GetExtension(url.AbsolutePath).ToLower();
+                        if (ext == ".png" || ext == ".jpg" || ext == ".bmp") return true;
+                    }
+                    return false;
+                })
+                .Select(m => m.Groups[0].Value)
+                .FirstOrDefault();
+        }
+
+    }
+}
---
+++ VirtualGym/GroupedItemsPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using VirtualGym.Data;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ApplicationSettings;
+using Windows.System;
+using Windows.Networking.Connectivity;
+using Windows.UI.Popups;
+
+// The Grouped Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234231
+
+namespace VirtualGym
+{
+    /// <summary>
+    /// A page that displays a grouped collection of items.
+    /// </summary>
+    public sealed partial class GroupedItemsPage : VirtualGym.Common.LayoutAwarePage
+    {
+        public GroupedItemsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override async void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            if (NetworkInformation.GetInternetConnectionProfile() == null
+|| NetworkInformation.GetInternetConnectionProfile().GetNetworkConnectivityLevel() != NetworkConnectivityLevel.InternetAccess)
+            {
+                var msg = new MessageDialog("The program need an internet connection to work. Please check it and restart the program.");
+                await msg.ShowAsync();
+            }
+            else
+            {
+                this.DefaultViewModel["Groups"] = DataSource.AllGroups;
+                DataSource.AddGroupForFeedAsync("http://www.sport-fitness-advisor.com/sports-fitness.xml");
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            SettingsPane.GetForCurrentView().CommandsRequested += Settings_CommandsRequested;
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            SettingsPane.GetForCurrentView().CommandsRequested -= Settings_CommandsRequested;
+            base.OnNavigatedFrom(e);
+        }
+
+        private void Settings_CommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            var viewPrivacyPage = new SettingsCommand("", "Privacy Statement", cmd =>
+            {
+                Launcher.LaunchUriAsync(new Uri("http://go.microsoft.com/fwlink/?LinkId=248681", UriKind.Absolute));
+            });
+            args.Request.ApplicationCommands.Add(viewPrivacyPage);
+        }
+
+        /// <summary>
+        /// Invoked when a group header is clicked.
+        /// </summary>
+        /// <param name="sender">The Button used as a group header for the selected group.</param>
+        /// <param name="e">Event data that describes how the click was initiated.</param>
+        void Header_Click(object sender, RoutedEventArgs e)
+        {
+            // Determine what group the Button instance represents
+            var group = (sender as FrameworkElement).DataContext;
+
+            // Navigate to the appropriate destination page, configuring the new page
+            // by passing required information as a navigation parameter
+            this.Frame.Navigate(typeof(GroupDetailPage), ((DataGroup)group).UniqueId);
+        }
+
+        /// <summary>
+        /// Invoked when an item within a group is clicked.
+        /// </summary>
+        /// <param name="sender">The GridView (or ListView when the application is snapped)
+        /// displaying the item clicked.</param>
+        /// <param name="e">Event data that describes the item clicked.</param>
+        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // Navigate to the appropriate destination page, configuring the new page
+            // by passing required information as a navigation parameter
+            var itemId = ((DataItem)e.ClickedItem).UniqueId;
+            this.Frame.Navigate(typeof(ItemDetailPage), itemId);
+        }
+    }
+}
